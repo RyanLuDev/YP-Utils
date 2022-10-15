d87d3121224db57653547f45a3615144c9f3aef8
Ryan Lu <lyydev@gmail.com>
2022-10-15 18:16:26 +0800

    [yfile]Rewrite

--- yputils/yfile.py
+++ yputils/yfile.py
@@ -2,57 +2,38 @@ import os
 import pathlib
 import hashlib
 
+IMG_TYPE = []
 
-def get_file_list(file_dir: str, f_exts: list[str]) -> list[str]:
-    f_exts = ['.' + t for t in f_exts]
-    files = []
 
-    for f in os.listdir(file_dir):
+def list_file(f_path: str, f_ext: list[str]) -> list[str]:
+    exts = ['.'+ext for ext in f_ext]
+    files: list[str] = []
+    for f in os.listdir(f_path):
         ext = ''.join(pathlib.Path(f).suffixes)
-        if ext in f_exts:
+        if ext in exts:
             files.append(f)
-
     return files
 
 
-def get_file_path_list(file_dir: str, f_exts: list[str]) -> list[str]:
-    return [os.path.join(file_dir, f) for f in get_file_list(file_dir, f_exts)]
+def list_file_path(f_path: str, f_ext: list[str]) -> list[str]:
+    return [os.path.join(f_path, f) for f in list_file(f_path, f_ext)]
 
 
-def get_img_list(img_dir: str, img_exts: list[str] = ['jpg', 'JPG', 'png', 'PNG']) -> list[str]:
-    img_exts = ['.'+ext for ext in img_exts]
-    imgs = []
-
-    for img in os.listdir(img_dir):
-        ext = ''.join(pathlib.Path(img).suffixes)
-        if ext in img_exts:
-            imgs.append(img)
-    return imgs
+def list_imgs(f_path: str, f_ext: list[str] = IMG_TYPE) -> list[str]:
+    return list_file(f_path, f_ext)
 
 
-def get_img_path_list(img_dir: str, img_exts: list[str] = ['jpg', 'JPG', 'png', 'PNG']) -> list[str]:
-    return [os.path.join(img_dir, img) for img in get_img_list(img_dir, img_exts=img_exts)]
-
-
-def is_dir_exists(path) -> bool:
-    return os.path.exists(path)
-
-
-def is_dir(dirpath: str) -> bool:
-    return os.path.isdir(dirpath)
-
-
-def is_file(path) -> bool:
-    return os.path.isfile(path)
-
-
-def is_file_exists(path) -> bool:
-    return os.path.exists(path)
+def lsit_imgs_path(f_path: str, f_ext: list[str] = IMG_TYPE) -> list[str]:
+    return list_file_path(f_path, f_ext)
 
 
 def mkdir(dirpath: str) -> None:
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
+
+
+def hash(f_name: str) -> str:
+    return ''
 
 
 def sha256(filename: str) -> str:
