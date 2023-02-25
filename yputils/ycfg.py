4f1217eb3d6dff18c4f7e131c7f4440cd59c41b2
Ryan Lu <lyydev@gmail.com>
2023-02-25 22:58:14 +0800

    fix load toml file error

--- yputils/ycfg.py
+++ yputils/ycfg.py
@@ -6,5 +6,5 @@ def load_toml(filepath):
     if not os.path.exists(filepath):
         return None
 
-    with open(filepath, 'r') as f:
-        return tomllib.load(f.read())
+    with open(filepath, 'rb') as f:
+        return tomllib.load(f)
